8e3330e9a72d5e8a4d1fa0c2c84581ada85697e1
U-MGC\shaynal <shaynal@ORW-1X4DYZ1.mgc.mentorg.com>
2016-08-21 22:01:42 -0700

    Added bom tools

---
+++ hardware/hl/bom/BOM.py
@@ -0,0 +1,405 @@
+
+import untangle, json
+import pickledb
+import urllib
+from decimal import Decimal
+
+
+class Quote:
+    def __init__(self,price,name,url,manufacturer,sku,mpn):
+        self.price = price
+        self.name = name
+        self.url = url
+        self.manufacturer = manufacturer
+        self.sku = sku
+        self.mpn = mpn
+
+    def __cmp__(self,other):
+        return cmp( (self.price,self.sku), (other.price,other.sku) )
+
+    def DebugPrint(self):
+        print "{0.2f:10} {1:20} {2:30} {3:30} {4}".format(self.price,self.sku,self.name,self.manufacturer,self.url)
+
+    def WikiLine(self):
+        if self.name == 'Digi-Key':
+            sku = '[{0}](http://www.digikey.com/product-search/en?keywords={0})'.format(self.sku)
+        else:
+            sku = self.sku 
+
+        seller = '[{0}]({1})'.format(self.name,self.url)
+
+        octoparturl = '[{0}](http://www.octopart.com/search?q={0})'.format(self.mpn)
+
+        return "{0} | {1} | {2} | {3} | {4:.2f}".format(self.manufacturer,octoparturl,seller,sku,self.price)
+
+## Special component quotes
+special = {
+    "HK4100F-DC12V-SHG":[Quote(Decimal(0.50),'*EBay','http://www.ebay.com','','HK4100F-DC12V-SHG','HK4100F-DC12V-SHG')],
+    "BN43-202":[Quote(Decimal(0.50),'*Kits and Parts','http://www.kitsandparts.com','','BN43-202','BN43-202')],
+    "BN43-2402":[Quote(Decimal(0.25),'*Kits and Parts','http://www.kitsandparts.com','','BN43-2402','BN43-2402')],
+    "T37-2":[Quote(Decimal(0.25),'*Kits and Parts','http://www.kitsandparts.com','','T37-2','T37-2')],
+    "T37-6":[Quote(Decimal(0.25),'*Kits and Parts','http://www.kitsandparts.com','','T37-6','T37-6')],
+    "RD15HVF1":[Quote(Decimal(4.00),'*AliExpress','http://www.aliexpress.com','Mitsubishi','RD15HVF1','RD15HVF1')],
+    "PCB":[Quote(Decimal(20.00),'*Tindie','http://www.tindie.com','Elecrow','PCB','PCB')],
+    "CASE":[Quote(Decimal(13.00),'*EBay','http://www.ebay.com','Various','','Aluminum Enclosure 150 105 55')],
+    "PROG":[Quote(Decimal(5.00),'*EBay','http://www.ebay.com','Various','','USB Blaster')]
+}
+
+
+
+class Octopart:
+    db = pickledb.load("octopart.db", False)
+
+    def __init__(self,key=''):
+        self.key = key 
+
+    def Dump(self):
+        Octopart.db.dump()      
+
+    def QueryWeb(self,mpns):
+        if len(mpns) > 20:
+            print "Octopart does not support more than 20 queries at a time for free"
+            return
+
+
+        queries = [{'mpn':l,'reference':l} for l in mpns]
+        
+        url = 'http://octopart.com/api/v3/parts/match?queries=%s' % urllib.quote(json.dumps(queries))
+        url += self.key
+        data = urllib.urlopen(url).read()
+        response = json.loads(data)
+
+        results = response['results']
+
+        for result in results:
+            Octopart.db.set(result['reference'],result)
+
+        self.Dump()
+
+    def DebugPrint(self,mpn):
+
+        def rec(o,indent):
+            if type(o) is dict:
+                for k,v in o.iteritems():
+                    if type(v) is dict or type(v) is list:
+                        print indent*' ',k,':'
+                        rec(v,indent+2)
+                    else:
+                        print indent*' ',k,':',v
+            elif type(o) is list:
+                for v in o:
+                    rec(v,indent+2)
+            else:
+                print indent*' ',o
+                
+        v = self.db.get(mpn)
+        if v == None:
+            print "MPN",mpn,"does not exist in DB"
+            return
+
+        rec(v,0)
+
+    def Quotes(self,mpn,quantity):
+
+        ##print "Quote for",mpn,quantity
+        quotes = []
+        v = self.db.get(mpn)
+        if v == None: 
+            ## Add any special quotes
+            if mpn in special:
+                quotes.extend(special[mpn])            
+            ##print "MPN",mpn,"does not exist in DB"
+            return quotes
+
+        items = v['items']
+
+        for item in items:
+            manufacturer = item['manufacturer']['name']
+            offers = item['offers']
+            for offer in offers:
+                ## Only accept if quotes are in USD
+                if 'USD' not in offer['prices'].keys(): continue
+                ## Only accept if enough parts are in stock
+                if offer['in_stock_quantity'] < quantity: continue
+                price = None
+                for price_tuple in offer['prices']['USD']:
+                    # Find correct price break
+                    if price_tuple[0] > quantity: break
+                    price = Decimal(price_tuple[1])
+                ## Only accept if a valid price exists
+                if not price: continue
+                quote = Quote(price,offer['seller']['name'],offer['seller']['homepage_url'],manufacturer,offer['sku'],mpn)
+                quotes.append(quote)
+
+        ## Add any special quotes
+        if mpn in special:
+            quotes.extend(special[mpn])
+        
+        return quotes
+
+octopart = Octopart()
+
+
+class Component:
+    def __init__(self,xml,hand):
+        self.xml = xml
+        self.option = ''
+        self.notes = ''
+        self.ext = ''
+        self.key = ''
+
+        self.value = self.xml.value.cdata
+
+        try:
+            for field in self.xml.fields.field:
+                if field['name'] == 'Option':
+                    self.option = field.cdata
+                    ##print "Found option",self.option
+                elif field['name'] == 'Notes':
+                    self.notes = field.cdata
+                elif field['name'] == 'Ext':
+                    self.ext = field.cdata
+                    ##print "Found ext",self.ext
+                elif field['name'] == 'Key':
+                    self.key = field.cdata
+                elif field['name'] == 'Hand' and hand:
+                    self.key = field.cdata
+
+        except:
+            pass
+
+        self.ref = self.xml['ref']
+        if self.key == '':
+            if self.ref[1].isdigit():
+                self.key = (self.ref[0] + " " + self.value + " " + self.ext).strip()
+            else:
+                self.key = (self.ref[0:2] + " " + self.value + " " + self.ext).strip()
+
+        ## Convert ref to sortable ascii,integer tuple
+        if self.ref[1].isdigit():
+            self.ref = self.ref[0],int(self.ref[1:])
+        else:
+            self.ref = self.ref[:2],int(self.ref[2:])
+
+    def DebugStr(self):
+        ##print "{0:15}{1:5}{2:20}{3:10}{4}".format(self.key,self.quantity,self.option,self.notes,self.mpns)
+        return "{0:20} {1:20}{2:10}{3:5}".format(self.key,self.option,self.notes,self.ref)
+
+
+class Part:
+
+    def __init__(self,component):
+        self.mpns = []
+        self.spec = ''
+        self.components = [component]
+
+    def AddComponent(self,component):
+        self.components.append(component)
+
+    def AddPartSel(self,v):
+        self.mpns = v['mpn']
+        self.spec = v['spec']
+        self.pins = v['pins']
+        self.assembly = v['assembly']
+
+    def Quantities(self):
+        unique = set([])
+        optional = 0
+        for c in self.components:
+            if c.option != '':
+                optional += 1
+                unique.add(c.option)
+
+        key = self.components[0].key
+        if "DNI" in key or "TP TEST" in key or "JNO" in key:
+            return (0,len(self.components),len(unique))
+        else:
+            return (1*(len(self.components)-optional),optional,len(unique))
+
+
+    def Quotes(self,prefer=None):
+        quantity = self.Quantities()
+        quotes = []
+        for mpn in self.mpns:
+            quotes.extend(octopart.Quotes(mpn,quantity[0]))
+        
+        if prefer:
+            quotes = [a for a in quotes if ((a.name in prefer) or (a.name[0] == '*'))]
+
+        quotes.sort()
+
+        return quotes
+
+    def WikiRefs(self,llen=20):
+
+        refs = [c.ref for c in self.components if c.option == '']
+        refs.sort()
+
+        if refs == []: return ''
+
+        res = '<p>'
+
+        i = 0
+        for ref in refs[:-1]:
+            if i > llen:
+                res = res + '</p><p>'
+                i = 0
+            ref = ref[0]+str(ref[1])
+            res = res + ref + ","
+            i = i + 1 + len(ref)
+
+        if i > llen:
+            res = res + '</p><p>'
+
+        res = res + refs[-1][0] + str(refs[-1][1]) + '</p>'
+
+        return res
+
+    def Sort(self):
+
+        self.components.sort(key=lambda x: x.ref)
+ 
+
+
+    def DebugPrint(self):
+        print "{0} {1:10} {2:20} {3}".format(self.components[0].DebugStr(),self.Quantities(),self.mpns,self.spec)
+
+    def PrintOptional(self):
+        for c in self.components:
+            if c.option != '': print c.ref,c.option
+
+
+
+
+class BOM:
+
+    def __init__(self,fn,hand=True):
+
+        self.hand = hand
+        ## Read components from KiCAD XML
+
+        kicad = untangle.parse(fn)
+
+        ml = kicad.export.components.comp
+
+        self.parts = {}
+  
+        for comp in ml:
+            c = Component(comp,self.hand)
+            if c.key in self.parts:
+                p = self.parts[c.key]
+                p.AddComponent(c)
+            else:
+                self.parts[c.key] = Part(c)
+
+        ## Pair with mpn
+        f = open("parts.json")
+        jo = json.load(f)
+        f.close()
+
+        for k,v in jo.iteritems():
+            if k not in self.parts:
+                print "WARNING: {0} in parts catalog but not used in design".format(k)
+            else:
+                ##if k == 'CASE':
+                ##    print "Adding key",k,v
+                p = self.parts[k]
+                p.AddPartSel(v)
+
+        for k,v in self.parts.iteritems():
+            v.Sort()
+
+    def DebugPrint(self):
+        keys = self.parts.keys()
+        keys.sort()
+        for k in keys:
+            v = self.parts[k]
+            v.DebugPrint()
+            quotes = v.Quotes(prefer=['Mouser','Digi-Key'])
+            ##print quotes
+            for q in quotes: q.DebugPrint()
+
+    def WikiPrint(self,prefer=None):
+        keys = self.parts.keys()
+        keys.sort(key=lambda x: self.parts[x].components[0].ref)
+        total = Decimal(0.0)
+
+        f = open("bom.md","w")
+
+        # itemspartspins
+        ipp = {
+            'SMT':(0,0,0),
+            'TH':(0,0,0),
+            'MTH':(0,0,0)
+        }
+
+        print >>f, "|  Reference  | Spec    | Manufacturer | Part Number | Supplier | SKU | Price | Qty | Total |"
+        print >>f, "| ----------- | ------- | ------------ | ----------- | -------- | --: | ----: | --: | ----: |"
+
+        for k in keys:
+            p = self.parts[k]
+
+            c1 = p.WikiRefs()
+
+            c3 = p.Quantities()[0]
+
+            if c3 == 0: continue
+
+            quotes = p.Quotes(prefer)
+            if quotes != []:
+                quote = quotes[0]
+                c2 = quote.WikiLine()
+                c4 = quote.price * c3
+            else:
+                print "ERROR: No quote for mpn",p.mpns,p.spec,p.components[0].ref
+                continue
+
+            items,parts,pins = ipp[p.assembly]
+            ipp[p.assembly] = items+1,parts+c3,pins+(p.pins*c3)
+
+            s = '| {0} | {1} | {2} | {3} | {4:.2f} |'.format(c1,p.spec,c2,c3,c4)
+            total = total + c4
+
+            print >>f,s
+
+        print >>f," * Total Price: ${0:.2f}".format(total)
+        print >>f," * Line Items"
+        print >>f,"  * SMT: {0}".format(ipp['SMT'][0])
+        print >>f,"  * Assembled TH: {0}".format(ipp['TH'][0])
+        print >>f,"  * Manual TH: {0}".format(ipp['MTH'][0])
+        print >>f,"  * Total: {0}".format(ipp['MTH'][0]+ipp['SMT'][0]+ipp['TH'][0])
+        print >>f," * Parts"
+        print >>f,"  * SMT: {0}".format(ipp['SMT'][1])
+        print >>f,"  * Assembled TH: {0}".format(ipp['TH'][1])
+        print >>f,"  * Manual TH: {0}".format(ipp['MTH'][1])
+        print >>f,"  * Total: {0}".format(ipp['MTH'][1]+ipp['SMT'][1]+ipp['TH'][1])
+        print >>f," * Pins"
+        print >>f,"  * SMT: {0}".format(ipp['SMT'][2])
+        print >>f,"  * Assembled TH: {0}".format(ipp['TH'][2])
+        print >>f,"  * Manual TH: {0}".format(ipp['MTH'][2])
+        print >>f,"  * Total: {0}".format(ipp['MTH'][2]+ipp['SMT'][2]+ipp['TH'][2])
+
+    def OctoPartUpdatePrices(self):
+
+        mpns = []
+        for key,part in self.parts.iteritems():
+            mpns.extend(part.mpns)
+
+        ## 20 parts at a time
+        for i in range(0,len(mpns),20):
+            sl = mpns[i:i+20]
+            octopart.QueryWeb(sl)
+
+    def PrintOptional(self):
+        keys = self.parts.keys()
+        keys.sort(key=lambda x: self.parts[x].components[0].ref)
+
+        for key in keys:
+            p = self.parts[key]
+            p.PrintOptional()
+
+
+  
+
+
+
